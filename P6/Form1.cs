728740122f297cccc6ccec781c1d6433d4caf71d
david <socialfreetime@gmail.com>
2023-09-18 10:22:49 +0200

    Agregar archivos de proyecto.

---
+++ P6/Form1.cs
@@ -0,0 +1,68 @@
+namespace P6
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            button1.BackColor = Color.Blue;
+            button2.BackColor = Color.Blue;
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            Close();
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+
+            Button boton = (Button)sender;
+            int nElementos = flowLayoutPanel1.Controls.Count;
+            if (boton.BackColor.Name == "Red")
+            {
+                boton.BackColor = Color.Green;
+         
+            }
+            else
+            {
+                boton.BackColor = Color.Red;
+               
+            }
+
+            int verde = 0;
+            int rojo = 0;
+            foreach (Control dato in flowLayoutPanel1.Controls) {
+                if (dato.BackColor.Name == "Red") {
+                    rojo++;
+                }
+                if (dato.BackColor.Name == "Green") {
+                    verde++;
+                }
+
+                if (nElementos == rojo) {
+                    button2.BackColor = Color.Red;
+                }
+            }
+
+        
+
+
+
+
+
+
+
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            foreach (Control dato in flowLayoutPanel1.Controls) {
+                dato.BackColor = Color.Green;
+            }
+        }
+    }
+
+   
+}
